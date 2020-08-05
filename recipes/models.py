73d2d4f6b59741543fcc87c57951eb793ed91867
Kevin Blount <kvbchamp@gmail.com>
2020-08-05 18:32:25 -0400

    completed author and recipes migration

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    bio = models.TextField()
+
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=50)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    description = models.TextField()
+    time_required = models.CharField(max_length=80)
+    instructions = models.TextField()
