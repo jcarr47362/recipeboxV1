19b18827a6a61c0379aebd9524c9a0dc2488d5b0
Kevin Blount <kvbchamp@gmail.com>
2020-08-05 19:20:33 -0400

    added str dunder method to models.py

--- recipes/models.py
+++ recipes/models.py
@@ -5,6 +5,9 @@ class Author(models.Model):
     name = models.CharField(max_length=100)
     bio = models.TextField()
 
+    def __str__(self):
+        return self.name
+
 
 class Recipe(models.Model):
     title = models.CharField(max_length=50)
@@ -12,3 +15,6 @@ class Recipe(models.Model):
     description = models.TextField()
     time_required = models.CharField(max_length=80)
     instructions = models.TextField()
+
+    def __str__(self):
+        return self.title
