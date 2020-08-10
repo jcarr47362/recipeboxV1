ea1633831f20de32f7924051e6f948feadfb187b
Kevin Blount <kvbchamp@gmail.com>
2020-08-10 17:41:13 -0400

    create AddAuthorForm and AddRecipeForm classes in forms.py

--- recipes/forms.py
+++ recipes/forms.py
@@ -1,1 +1,12 @@
 from django import forms
+
+
+class AddAuthorForm(forms.Form):
+    name = forms.CharField(label='Name', max_length=50)
+    bio = forms.CharField(widget=forms.Textarea)
+
+
+class AddRecipeForm(forms.Form):
+    title = forms.CharField(max_length=50)
+    author = ''
+    description = forms.CharField(widget=forms.Textarea())
